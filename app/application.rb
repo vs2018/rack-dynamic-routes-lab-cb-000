9539047712b71d711486dec7193e0d82f96559c0
Vishal Sharma <vsh1819@gmail.com>
2018-07-10 15:58:21 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,9 +6,11 @@ class Application
  
       if req.path.match(/items/)
       item = req.path.split("/items/").last 
+      
       if item
         resp.write item.price
       else
+        resp.write "Item not found"
         resp.status = 400
       end
       
